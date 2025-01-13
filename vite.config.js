b31a358bcecf17cc26872ed831ff89dc3371850c
정민규 <jmg2165@naver.com>
2025-01-13 16:52:37 +0900

    chore : config.js 파일에대해 devDependencies 의존성 import 허용 ( airbnb-rule 예외처리)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    esbuild: {
-        jsxFactory: "createElement",
-        jsxFragment: "Fragment",
-    },
+  esbuild: {
+    jsxFactory: 'createElement',
+    jsxFragment: 'Fragment',
+  },
 });
