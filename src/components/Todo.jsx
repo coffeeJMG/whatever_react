67843f8ba5274df7a03b5b15d133efa44ad31ca6
정민규 <jmg2165@naver.com>
2025-01-25 00:32:17 +0900

    chore : eslint, prettier 세팅 및 SonarLinst 수정

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,74 +1,64 @@
-import useState from "../hook/useState";
-import styles from "./style.module.css"
+import useState from '../hook/useState';
+import styles from './style.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const Todo = () => {
-  const [value, setValue] = useState("")
-  const [todos, setTodos] = useState([])
-
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([]);
 
   const handleInput = (e) => {
     setValue(e.target.value);
   };
 
-
   const handleAddTodo = () => {
-
-      if (value.trim() !== '') {
-
-          setTodos([...todos, value]);
-          setValue("");
-
-      }
-  }
+    if (value.trim() !== '') {
+      setTodos((prev) => [...prev, { id: uuidv4(), text: value }]);
+      setValue('');
+    }
+  };
 
   const handleKeyDown = (e) => {
-
     if (e.key === 'Enter') {
       handleAddTodo();
     }
-
   };
 
-  const handleRemoveTodo = (index) => {
-
-    // 현재 todos 배열에서 클릭한 인덱스의 항목만 제거
-    setTodos(todos => {
-      return todos.filter((todo, currentIndex) => {
-        return currentIndex !== index; // 클릭한 index와 다른 항목만 남김
-      });
-    });
-   };
+  const handleRemoveTodo = (id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
 
   return (
-      <>
-          <div className={styles.container}>
-              <h1>Todo List</h1>
-              <div className={styles.todoForm}>
-                  <input
-                      type="text"
-                      onInput={handleInput}
-                      value={value}
-                      onKeyDown={handleKeyDown}
-                      placeholder="할 일을 입력하세요"
+    <div className={styles.container}>
+      <h1>Todo List</h1>
+      <div className={styles.todoForm}>
+        <input
+          type="text"
+          onInput={handleInput}
+          value={value}
+          onKeyDown={handleKeyDown}
+          placeholder="할 일을 입력하세요"
+        />
+        <button type="button" onClick={handleAddTodo}>
+          입력
+        </button>
+      </div>
 
-                  />
-                  <button type="button" onClick={handleAddTodo}>
-                      입력
-                  </button>
-              </div>
+      <ul className={styles.list}>
+        {todos.map((todo) => (
+          <li key={todo.id} className={styles.item}>
+            {todo.text}
+            <button onClick={() => handleRemoveTodo(todo.id)}>
+              <img
+                src="/close.svg"
+                alt="리스트 삭제"
+                className={styles.closeImg}
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-              <ul className={styles.list}>
-                  {todos.map((todo, index) => (
-                      <li key={index} className={styles.item}>
-                          {todo}
-                          <img src="../public/close.svg" alt="리스트 삭제" className={styles.closeImg} onClick={()=>handleRemoveTodo(index)}/>
-                      </li>
-
-                  ))}
-              </ul>
-          </div>
-      </>
-  )
-}
-
-export default Todo
+export default Todo;
